83d2f06493b02013056626a7e7a2a81af8c1bf70
Donnie Karns <Donnie Karns>
2018-01-16 15:57:19 -0800

    Added spotify playlist, zwave sensor, various automations

--- camera/scripts/usb_photo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WIDTH=640
-HEIGHT=480
-PICTURE_PATH=/home/homeassistant/.homeassistant/camera
-VIDEO_ID=$1
-IMAGE_NAME=$2
-
-/usr/bin/fswebcam -d /dev/video$1 -r $WIDTHx$HEIGHT $PICTURE_PATH/$IMAGE_NAME
